b203395d6290236298053119a012f63054c4da7a
DonKingFrog <don.tgh1@gmail.com>
2025-02-16 17:12:54 +0200

    e

--- src/index.js
+++ src/index.js
@@ -2,62 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './colors.css';
-import {Home, Marketplace, Blog, Contact, Newsroom, Product, Discord, About, FAQ, Services} from './App';
+import { Home, Marketplace, Blog, Contact, Newsroom, Product, Discord, About, FAQ, Services } from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider, Route  } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-
-  {
-    path: "/marketplace",
-    element: <Marketplace />,
-  },
-
-  {
-    path: "/product/:productId",
-    element: <Product />
-  },
-
-  {
-    path: "/blog",
-    element: <Blog />,
-  },
-
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-
-  {
-    path: "/newsroom/:postId",
-    element: <Newsroom />,
-  },
-
-  {
-    path: "/about",
-    element: <About />
-  },
-
-  {
-    path: "/faq",
-    element: <FAQ />
-  },
-
-  {
-    path: "/discord",
-    element: <Discord />
-  }
-])
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newsroom/:postId" element={<Newsroom />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/discord" element={<Discord />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
