8eccf51f21ba0519be0806830d567baf95ab4e2d
DonKingFrog <don.tgh1@gmail.com>
2025-02-17 12:34:58 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './colors.css';
-import { Routes } from "./Routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage, MarketplacePage, BlogPage, ContactPage, NewsroomPage, ProductPage, DiscordPage, AboutPage, FAQPage } from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Routes />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Router>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/marketplace" element={<MarketplacePage />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/newsroom/:postId" element={<NewsroomPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/discord" element={<DiscordPage />} />
+      </Routes>
+  </Router>
 )
 
 
