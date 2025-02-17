fe4fb6026f02d43585e0755e4cb662ede8f52a2d
DonKingFrog <don.tgh1@gmail.com>
2025-02-17 12:45:00 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './colors.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage, MarketplacePage, BlogPage, ContactPage, NewsroomPage, ProductPage, DiscordPage, AboutPage, FAQPage } from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,15 +11,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Router basename='/react'>
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/marketplace" element={<MarketplacePage />} />
-          <Route path="/product/:productId" element={<ProductPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/newsroom/:postId" element={<NewsroomPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/faq" element={<FAQPage />} />
-          <Route path="/discord" element={<DiscordPage />} />
+          <Route path="" element={<HomePage />} />
+          <Route path="marketplace" element={<MarketplacePage />} />
+          <Route path="product/:productId" element={<ProductPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="newsroom/:postId" element={<NewsroomPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="faq" element={<FAQPage />} />
+          <Route path="discord" element={<DiscordPage />} />
       </Routes>
   </Router>
 )
