3ed00d4ec5a59f0a6f52b3fce611fcc06ac2db23
DonKingFrog <don.tgh1@gmail.com>
2025-02-17 12:11:30 +0200

    Commit

--- src/Routes.js
+++ src/Routes.js
@@ -10,17 +10,17 @@ window.$docsify = {
 
 export const Routes = () => {
     return (
-        <Router basename="/react">
+        <Router>
             <Switch>
-                <Route exact path="/" element={<HomePage />} />
-                <Route exact path="/marketplace" element={<MarketplacePage />} />
-                <Route exact path="/product/:productId" element={<ProductPage />} />
-                <Route exact path="/blog" element={<BlogPage />} />
-                <Route exact path="/newsroom/:postId" element={<NewsroomPage />} />
-                <Route exact path="/contact" element={<ContactPage />} />
-                <Route exact path="/about" element={<AboutPage />} />
-                <Route exact path="/faq" element={<FAQPage />} />
-                <Route exact path="/discord" element={<DiscordPage />} />
+                <Route exact path="/react/" element={<HomePage />} />
+                <Route exact path="/react/marketplace" element={<MarketplacePage />} />
+                <Route exact path="/react/product/:productId" element={<ProductPage />} />
+                <Route exact path="/react/blog" element={<BlogPage />} />
+                <Route exact path="/react/newsroom/:postId" element={<NewsroomPage />} />
+                <Route exact path="/react/contact" element={<ContactPage />} />
+                <Route exact path="/react/about" element={<AboutPage />} />
+                <Route exact path="/react/faq" element={<FAQPage />} />
+                <Route exact path="/react/discord" element={<DiscordPage />} />
             </Switch>
         </Router>
     );
