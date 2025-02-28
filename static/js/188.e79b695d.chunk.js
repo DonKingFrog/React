87a15beccd61dcf88397182eb485faa7e024a990
DonKingFrog <don.tgh1@gmail.com>
2025-02-28 20:03:35 +0200

    Updates

--- static/js/188.e79b695d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfinance=self.webpackChunkfinance||[]).push([[188],{188:(e,n,c)=>{c.r(n),c.d(n,{default:()=>s});const s={}}}]);
-//# sourceMappingURL=188.e79b695d.chunk.js.map
--- static/js/791.1c765e67.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfinance=self.webpackChunkfinance||[]).push([[791],{791:(e,n,c)=>{c.r(n),c.d(n,{default:()=>s});const s={}}}]);
-//# sourceMappingURL=791.1c765e67.chunk.js.map
