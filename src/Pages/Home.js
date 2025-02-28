4287fce8717bc7eb6b3e3b3963d37b6fb8dba490
DonKingFrog <don.tgh1@gmail.com>
2025-02-28 20:30:46 +0200

    e

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -137,7 +137,7 @@ function RenderHomePage() {
 
                         <button className="faqItem" onClick={(event) => toggleFAQActive(event)}>
                             <h1>Can I sell/redistribute any Dev Equip product?</h1>
-                            <p>Only if you're product is <b>FREE + Open Sourced + Apply Appropriate Credit to DonKingFrog/Dev Equip</b>.</p>
+                            <p>Unfortunately you're unable to sell any of our products but can distribute for free as long as you obide by the license agreement provided in the store page for each product.</p>
 
                             <svg id="plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
                             <svg id="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
